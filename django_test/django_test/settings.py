510104703a9966d470c75cc087ba656854e3a233
DevinChenChuang <692653284@qq.com>
2021-12-02 00:39:39 +0800

    mysql client的配置

--- django_test/django_test/settings.py
+++ django_test/django_test/settings.py
@@ -89,6 +89,12 @@ WSGI_APPLICATION = 'django_test.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'django_test/my.cnf'),
+        },
+    },
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
